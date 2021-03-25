b8255422b9330fda7db16b8f32176e8fd725efba
Ashley Stith <ashleyc.stith@gmail.com>
2021-03-25 00:50:26 -0400

    Updates to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/db.js",
     "/index.js",
     "/manifest.webmanifest",
-    "/favicon.ico",
+    // "/favicon.ico",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
   ];
