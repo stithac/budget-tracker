1577f5a760dbe5f2f3601977fbbb65c797459e50
Ashley Stith <ashleyc.stith@gmail.com>
2021-03-25 21:55:33 -0400

    Minor update to service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,8 @@ const FILES_TO_CACHE = [
     "/index.js",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
+    "/icons/icon-512x512.png",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 
   ];
 
